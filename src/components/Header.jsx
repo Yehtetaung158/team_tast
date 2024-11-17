97736640f557a014a94de750ce6689a871aad66d
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2024-11-18 02:43:56 +0630

    nav bar is done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -77,7 +77,7 @@ const Header = () => {
                         to={"/about"}
                         className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                       >
-                        About
+                        About us
                       </NavLink>
                       <NavLink
                         to={"/blog"}
