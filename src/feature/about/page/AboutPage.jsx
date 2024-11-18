33667ba4df58daeaf334fe7cb658aee5fc342f51
khantMinHtut <khantminhtut009@gmail.com>
2024-11-18 19:23:39 +0630

    feat: develop about-us page

--- src/feature/about/page/AboutPage.jsx
+++ src/feature/about/page/AboutPage.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div>
+      AboutPage
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ex
+        itaque, quam iste voluptate quas dignissimos? Mollitia commodi omnis
+        iusto, aliquid recusandae quae deserunt ea quia! Earum porro nihil ut.
+      </p>
+    </div>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
