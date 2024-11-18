3f746528e67bec7b627f8902117c05d18944aedd
zett21 <rtt2001xx@gmail.com>
2024-11-18 23:37:02 +0630

    svg fixed

--- src/feature/home/components/HeroSection.jsx
+++ src/feature/home/components/HeroSection.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import img1 from "../../../assets/HomeIMG/heroIMG.png";
-import img2 from "../../../assets/HomeIMG/kite.png"
+import img2 from "../../../assets/HomeIMG/kite.png";
 import { MoveRight } from "lucide-react";
 
 const HeroSection = () => {
   return (
-    <div className=" w-full flex flex-col homebg text-center p-2 ">
+    <div className=" w-full flex flex-col homebg text-center p-5 relative ">
       <div className="lg:w-1/2  mx-auto py-[80px] space-y-[24px] ">
-        <div className="font-miniHeader text-paragraph">
+        <div className="font-miniHeader_style text-paragraph">
           <h5>The Art of UI Design</h5>
         </div>
         <div className=" bitter text-[40px] lg:text-[60px]">
@@ -17,23 +17,24 @@ const HeroSection = () => {
             help.
           </h1>
         </div>
-        <div className=" text-center text-sm lg:text-md text-paragraph px-16">
+
+        <div className=" text-center text-sm lg:text-md text-paragraph px-16 ">
           <p>
             We’ve been told it is possible to revolutionize the payment
             industry. We have not reinvented the wheel, we decide
           </p>
         </div>
-        <div className="w-[289px] h-[46px] flex gap-5 items-center justify-center mx-auto  ">
-          <button
 
+        <div className="w-full flex justify-center items-center gap-5    ">
+          <button
             type="button"
-            className="text-paragraph hover:text-white border border-HLHeader hover:bg-HLHeader font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2  transition-all duration-150 delay-75"
+            className="text-HLHeaderColor border border-HLHeaderColor font-medium rounded-lg text-sm px-5 py-2.5  hover:bg-white hover:text-paragraphColor"
           >
             Learn More
           </button>
           <button
             type="button"
-            className="focus:outline-none text-white hover:text-paragraph bg-HLHeader hover:bg-white border hover:border-HLHeader  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2  transition-all duration-150 delay-75"
+            className="focus:outline-none text-white bg-HLHeaderColor font-medium rounded-lg text-sm px-5 py-2.5   transition-all duration-75  hover:text-HLHeaderColor hover:bg-white hover:border-HLHeaderColor"
           >
             Get Start <MoveRight className="size-5 inline-flex gap-2" />
           </button>
@@ -42,7 +43,7 @@ const HeroSection = () => {
           <img src={img1}></img>
         </div>
       </div>
-      <div className="inline-flex justify-end max-md:hidden lg:-translate-y-[600px] lg:-translate-x-40">
+      <div className=" w-fit h-fit max-lg:hidden absolute right-32 top-[350px] ">
         <img src={img2} alt="" />
       </div>
     </div>
